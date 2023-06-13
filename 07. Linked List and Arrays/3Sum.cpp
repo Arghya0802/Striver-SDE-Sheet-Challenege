784a8ec06af74bc3e876038a00139f93c87b2f71
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-13 22:36:15 +0530

    Update 3Sum.cpp

--- 07. Linked List and Arrays/3Sum.cpp
+++ 07. Linked List and Arrays/3Sum.cpp
@@ -10,5 +10,45 @@
 6. To skip duplicates we use, while(i + 1 < n && nums[i] == nums[i + 1) i++; 
 */
 
+#include <bits/stdc++.h> 
+
+vector<vector<int>> findTriplets(vector<int> &arr, int n, int K) 
+{
+	sort(arr.begin() , arr.end() ) ;
+
+	vector<vector<int>> ans ;
+
+	for(int i = 0 ; i < n ; i++)
+	{
+		int target = K - arr[i] ;
+
+		int low = i + 1 ; int high = n - 1 ;
+
+		while(low < high)
+		{
+			int sum = arr[low] + arr[high] ;
+
+			if(sum == target)
+			{
+				int a = arr[i] , b = arr[low] , c = arr[high] ;
+
+				ans.push_back( {a , b , c} ) ;
+
+				while(low < high && b == arr[low] ) low++ ;
+
+				while(low < high && c == arr[high] ) high-- ;
+			}
+
+			else if(sum < target)  low++ ;
+
+			else high-- ;
+		}
+
+		while(i + 1 < n && arr[i] == arr[i + 1] ) i++ ;
+	}
+
+	return ans ;
+}
+
 // Time Complexity:  O(N ^ 2)
 // Space Complexity: O(1)

4c307cd429e0ed6f23914a2eab9fd31f62df3981
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-18 00:00:00 +0530

    Create Combination Sum II.cpp

---
+++ 09. Recursion/Combination Sum II.cpp
@@ -0,0 +1,9 @@
+// LeetCode Link:  https://leetcode.com/problems/combination-sum-ii/
+// -----------------------------------------------------------------
+
+/* Approach [ Similar to Subset Sum - II ]
+1. Similar to Combination Sum - I
+2. We pick and skip to generate all the subsets.
+3. While skipping, we skip all the occurrences of the current element.
+4. We only pick those subsets whose sum is equal to Target or K
+*/

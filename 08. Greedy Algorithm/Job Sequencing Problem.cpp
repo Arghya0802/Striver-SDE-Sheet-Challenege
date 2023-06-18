1d64c6d162864b2c227bf56140448e126ec0c2aa
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-18 23:47:44 +0530

    Update Job Sequencing Problem.cpp

--- 08. Greedy Algorithm/Job Sequencing Problem.cpp
+++ 08. Greedy Algorithm/Job Sequencing Problem.cpp
@@ -9,5 +9,40 @@
 5. Otherwise if our index pointer becomes equal to 0, it means we cannot finish the current job before its deadLine, so we ignore it.
 */
 
+#include <bits/stdc++.h> 
+
+static bool cmp(vector<int> &A , vector<int> &B)
+{
+    return A[1] > B[1] ;
+}
+
+int jobScheduling(vector<vector<int>> &jobs)
+{
+    int n = jobs.size() ;
+
+    sort(jobs.begin() , jobs.end() , cmp) ; // We sort the given vector based on descending order of Profits
+
+    int maxDead = 0 ;  int maxProfit = 0 ;
+
+    for(int i = 0 ; i < n ; i++)  maxDead = max(maxDead , jobs[i][0]) ;
+
+    vector<int> days(maxDead + 1 , -1) ; // We create an array days[] of size (maxDead + 1) to keep track of which job we are doing on what day
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        int deadLine = jobs[i][0] ;
+       
+        while(deadLine > 0 && days[deadLine] != -1)  deadLine-- ;
+
+        if(deadLine == 0)  continue ; // We have found no days remaining to do the current job before its deadline
+
+        days[deadLine] = i + 1 ; // We include the Current Job and mark it in our days[] array
+        maxProfit += jobs[i][1] ; // And we also include its profit
+    }
+
+    return maxProfit ;
+}
+
+
 // Time Complexity:  O(N * Log N) + O(N * MaxDeadLine)
 // Space Complexity: O(MaxDeadLine)

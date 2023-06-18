2572a7bee101700cac95df4384d0537bb8c4c882
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-18 23:48:39 +0530

    Update Palindrome Partitioning.cpp

--- 09. Recursion/Palindrome Partitioning.cpp
+++ 09. Recursion/Palindrome Partitioning.cpp
@@ -9,5 +9,57 @@
 5. After the recursion call has returned, we have to backtrack and undo the change- remove the prefix string from our container[] so that don't include it in other possible partitioning sequences.
 */
 
+#include <bits/stdc++.h>
+
+bool isPalindrome(string &wd)
+{
+    int low = 0 ;  int high = wd.size() - 1 ;
+
+    while(low <= high)
+    {
+        if(wd[low] != wd[high])  return false ;
+
+        low++ ; high-- ; 
+    }
+
+    return true ;
+}
+
+void palindromePartition(int ind , int n , vector<string> &container , string &s , vector<vector<string>> &ans)
+{
+    if(ind >= n) // Base Case
+    {
+        ans.push_back(container) ;  return ;
+    }
+
+    string prefixStr = "" ; 
+
+    for(int i = ind ; i < n ; i++)
+    {
+        prefixStr += s[i] ;
+
+        if(isPalindrome(prefixStr) )
+        {
+            container.push_back(prefixStr) ;
+
+            palindromePartition(i + 1 , n , container , s , ans) ;
+
+            container.pop_back() ;
+        }
+    }
+
+    return ;
+}
+
+vector<vector<string>> partition(string &s) 
+{
+    vector<vector<string>> ans ; // Stores all the Palindrome Partitions
+    vector<string> container ; // Stores the current sequence of Palindrome Partitions
+
+    palindromePartition(0 , s.size() , container , s , ans) ;
+
+    return ans ;
+}
+
 // Time Complexity:  O(2 ^ N * N / 2) [2 ^ N is to generate all the substrings, N / 2 is to check if they are Palindrome or not]
 // Space Complexity: O(N) [Recursive Stack Space & Ignoring space taken ans[][] and Container vectors respectuvely] 

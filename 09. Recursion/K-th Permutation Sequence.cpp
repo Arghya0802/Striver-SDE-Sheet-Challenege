48e6e97ff78b1c0d4037e0a255d395d8ba126cf6
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-18 23:49:45 +0530

    Update K-th Permutation Sequence.cpp

--- 09. Recursion/K-th Permutation Sequence.cpp
+++ 09. Recursion/K-th Permutation Sequence.cpp
@@ -10,5 +10,37 @@
 6. We break out of the loop, whenever our nums become empty or nums.size() equals 0.
 */
 
+string kthPermutation(int n, int k) 
+{
+    k-- ;  // We are considering 0-based indexing 
+
+    int fact = 1 ;
+
+    vector<int> nums ;  
+
+    for(int i = 1 ; i < n ; i++)
+    {
+        nums.push_back(i) ;  fact *= i ;
+    }
+
+    nums.push_back(n) ;   string ans = "" ;  
+
+    while(true)
+    {
+        int ind = k / fact ; // This gives us the Index of the Current Element
+
+        ans += to_string(nums[ind]) ;
+
+        nums.erase(nums.begin() + ind) ; // We erase it from our nums[] vector
+
+        if(nums.size() == 0) break ;
+
+        k %= fact ;  fact /= nums.size() ;
+    }
+
+    return ans ;
+}
+
+
 // Time Complexity:  O(N ^ 2) [To iterate over all the elements once and delete the element after we have taken it]
 // Space Complexity: O(N) [We have taken extra nums[] vector]

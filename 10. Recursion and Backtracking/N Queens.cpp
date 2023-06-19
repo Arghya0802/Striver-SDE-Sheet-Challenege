d1dd52673f4d0ad494a826b8dd9e5b8bf25e0491
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-20 00:11:56 +0530

    Update N Queens.cpp

--- 10. Recursion and Backtracking/N Queens.cpp
+++ 10. Recursion and Backtracking/N Queens.cpp
@@ -1,12 +1,17 @@
 // LeetCode Link:  https://leetcode.com/problems/n-queens/description/
 // --------------------------------------------------------------------
 
-/* Approach
+/* Approach [ Recursion & Backtracking]
+1. We need to make such a configuration that we could place N Queens inside our ChessBoard without them canceling each other.
+2. We need to try all such possible configurations and use the function isPossible() which checks if we can place our Queen at any cell (r,c).
+3. If true, we place the Queen and ask for recursion to do the rest of the task.
+4. If false, we look for other configurations
+5. Whenever we have placed N Queens, we put that configuration inside our ans[] vector and return back {Base Case}
 */
 
 #include<bits/stdc++.h>
 
-void makeChessBoard(vector<vector<int>> &board , int n , vector<vector<int>> &ans)
+void makeChessBoard(vector<vector<int>> &board , int n , vector<vector<int>> &ans) // Makes our ChessBoard and puts it inside our ans[][] vector
 {
     vector<int> temp ;
 
@@ -18,7 +23,7 @@ void makeChessBoard(vector<vector<int>> &board , int n , vector<vector<int>> &an
     ans.push_back(temp) ;
 }
 
-bool rowSafe(int row , int n , vector<vector<int>> &board)
+bool rowSafe(int row , int n , vector<vector<int>> &board) // Checks if the Row is Safe for the Queen to be placed or not
 {
     for(int col = 0 ; col < n ; col++)
     {
@@ -28,7 +33,7 @@ bool rowSafe(int row , int n , vector<vector<int>> &board)
     return true ;
 }
 
-bool colSafe(int col , int n , vector<vector<int>> &board)
+bool colSafe(int col , int n , vector<vector<int>> &board) // Checks if the Column is Safe for the Queen to be placed or not
 {
     for(int row = 0 ; row < n ; row++)
     {
@@ -38,7 +43,7 @@ bool colSafe(int col , int n , vector<vector<int>> &board)
     return true ;
 }
 
-bool diagonalSafe(int row , int col , int n , vector<vector<int>> &board)
+bool diagonalSafe(int row , int col , int n , vector<vector<int>> &board) // Checks if the Diagonals are Safe for the Queen to be placed or not
 {
     // As we are moving from Top to Bottom, so we need to check for Upper Part of Left and Right Diagonals only
     
@@ -65,7 +70,7 @@ bool diagonalSafe(int row , int col , int n , vector<vector<int>> &board)
     return true ;
 }
 
-bool isOkay(int row , int col , int n , vector<vector<int>> &board)
+bool isPossible(int row , int col , int n , vector<vector<int>> &board) // Calls rowSafe(), colSafe() and diagonalSafe() and returns accordingly
 {
     bool c1 = rowSafe(row , n , board) ;
 
@@ -73,25 +78,25 @@ bool isOkay(int row , int col , int n , vector<vector<int>> &board)
 
     bool c3 = diagonalSafe(row , col , n , board) ;
 
-    return c1 && c2 && c3 ;
+    return c1 && c2 && c3 ; // Returns true if all the values are true else returns false
 }
 
-void solve(int row , int n , vector<vector<int>> &board , vector<vector<int>> &ans)
+void solveQueens(int row , int n , vector<vector<int>> &board , vector<vector<int>> &ans)
 {
-    if(row == n)
+    if(row == n) // Base Case
     {
         makeChessBoard(board , n , ans) ;  return ;
     }
 
     for(int col = 0 ; col < n ; col++)
     {
-        if(isOkay(row , col , n , board) == true)
+        if(isPossible(row , col , n , board) == true) // Checking if it's possible to put a Queen at the current configuration or not
         {
-            board[row][col] = 1 ;
+            board[row][col] = 1 ; // Puts Queen at (row, col) position
 
-            solve(row + 1 , n , board , ans) ;
+            solveQueens(row + 1 , n , board , ans) ; // Asks recursion to do rest of the task
 
-            board[row][col] = 0 ;
+            board[row][col] = 0 ; // Backtracks and undo the change
         }
     }
 
@@ -103,7 +108,7 @@ vector<vector<int>> solveNQueens(int n)
     vector<vector<int>> board(n , vector<int> (n , 0) ) ;
     vector<vector<int>> ans ;
 
-    solve(0 , n , board , ans) ;
+    solveQueens(0 , n , board , ans) ;
 
     return ans ;
 }

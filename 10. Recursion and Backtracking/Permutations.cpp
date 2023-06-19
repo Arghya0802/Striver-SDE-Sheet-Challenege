5c15c04e1f30db40c55555122db2ff6902f93e0e
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-19 23:54:51 +0530

    Update Permutations.cpp

--- 10. Recursion and Backtracking/Permutations.cpp
+++ 10. Recursion and Backtracking/Permutations.cpp
@@ -1,7 +1,11 @@
 // Link:  https://leetcode.com/problems/permutations/
 // --------------------------------------------------
 
-/* Approach  
+/* Approach [ Generate all Permutations by swapping Indices ]
+1. As we are asked to generate all permutations, we can do so by swapping indices - s[i] and s[ind]
+2. ind is the parameter that we will pass to our recursive function
+3. We run a for loop from i = ind till i < n and for every index s[i], we swap it with s[ind] and call recur(ind + 1, s) and ask it to generate rest of the permutations
+4. Whenever our ind becomes equal to s.size(), we put it inside our ans[] vector
 */
 
 #include <bits/stdc++.h> 
@@ -35,4 +39,4 @@ vector<string> findPermutations(string &s)
 }
 
 // Time Complexity:  O(N!)
-// Space Complexity: O(N!)
+// Space Complexity: O(N) [Recursive Stack Space] + O(N!) [For Storing all the Permutations inside ans[] vector]

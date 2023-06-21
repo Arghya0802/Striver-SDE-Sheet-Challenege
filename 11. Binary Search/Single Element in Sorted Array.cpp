fbe6c901d5b66e4e476c04606a7e3f4674980e67
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-21 23:27:59 +0530

    Create Single Element in Sorted Array.cpp

---
+++ 11. Binary Search/Single Element in Sorted Array.cpp
@@ -0,0 +1,9 @@
+// LeetCode: https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+// ------------------------------------------------------------------------------------
+
+/* Approach
+*/
+
+
+// Time Complexity:  O(Log N)
+// Space Complexity: O(1)

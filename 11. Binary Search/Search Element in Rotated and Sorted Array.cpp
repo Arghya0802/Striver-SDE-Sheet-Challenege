e3036363828234dcdab9e2f379fca5563b81aa8b
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-06-22 23:44:51 +0530

    Create Search Element in Rotated and Sorted Array.cpp

---
+++ 11. Binary Search/Search Element in Rotated and Sorted Array.cpp
@@ -0,0 +1,9 @@
+// LeetCode Link:  https://leetcode.com/problems/search-in-rotated-sorted-array/
+// -----------------------------------------------------------------------------
+
+/* Approach
+*/
+
+
+// Time Complexity:  O(Log N)
+// Space Complexity: O(1)
